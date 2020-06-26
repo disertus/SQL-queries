c329f51a31449f6f85622c1fc18b6c013d7bc7b1
disertus <61247961+disertus@users.noreply.github.com>
2020-06-26 22:05:01 +0300

    Update Refresher-Exercise1-SQL.sql

--- Refresher-Exercise1-SQL.sql
+++ Refresher-Exercise1-SQL.sql
@@ -2,7 +2,18 @@
 
 -- selection of single 'field' or 'column' (terms used interchangably)
 SELECT name FROM people;
-SELECT title FROM films;
-SELECT release_year FROM films;
 
---
+-- selection of two and more fileds
+SELECT title, release_year FROM films;
+
+-- selection of all fields 'limited' to first 10 rows 
+SELECT * FROM people LIMIT 10;
+
+-- selection of unique entries with DISTINCt for duplicated entries
+SELECT DISTINCT language FROM films;
+
+-- COUNT aggregate function; Count all entries:
+SELECT COUNT(*) FROM people;
+-- Count all unique entries in a column:
+SELECT COUNT(DISTINCT birthdate) FROM people;
+
