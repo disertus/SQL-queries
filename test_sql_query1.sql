25d7b2de7dc31e5a504068b7f0f49aa73299a1da
disertus <61247961+disertus@users.noreply.github.com>
2020-06-26 10:32:34 +0300

    Create test_sql_query1.sql

---
+++ test_sql_query1.sql
@@ -0,0 +1,7 @@
+SELECT COUNT(news_posts.source_url) as frequency,
+      news_posts.source_url,
+      news_sources.location_name
+FROM news_posts
+INNER JOIN news_sources ON news_posts.source_url = news_sources.url
+GROUP BY location_name, source_url
+ORDER BY location_name, frequency DESC;
