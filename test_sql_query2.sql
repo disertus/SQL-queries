27b341c94ae916b37cfddf76c34ea434ee9f8519
disertus <61247961+disertus@users.noreply.github.com>
2020-06-26 10:33:12 +0300

    Create test_sql_query2.sql

---
+++ test_sql_query2.sql
@@ -0,0 +1,8 @@
+SELECT COUNT(news_posts.source_url) as frequency,
+      DATE(news_posts.added_at) as posts_date,
+      news_sources.location_name
+FROM news_posts
+INNER JOIN news_sources ON news_posts.source_url = news_sources.url
+WHERE location_name = 'kyiv'
+GROUP BY posts_date, location_name
+ORDER BY posts_date DESC;
