cc70a642d874244a6cc7948883cfe8e49ce26ccc
disertus <61247961+disertus@users.noreply.github.com>
2020-06-28 15:03:42 +0300

    Update Refresher-exercise2.sql

--- refresher-exercises/Refresher-exercise2.sql
+++ refresher-exercises/Refresher-exercise2.sql
@@ -1,1 +1,13 @@
 -- Queries of intermediate complexity with commented explanations for myself
+
+
+-- Aggregate functions can be combined with the WHERE clause to gain further insights from data
+SELECT SUM(budget)
+FROM films
+WHERE release_year >= 2010; -- returns the sum budget of fimls released after/in 2010
+
+SELECT AVG(gross)
+FROM films
+WHERE title LIKE 'A%'
+AND release_year BETWEEN 2000 AND 2012;
+
