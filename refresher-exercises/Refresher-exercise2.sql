a71c87e6afc9c737095d9d5172555a1045b3af38
disertus <61247961+disertus@users.noreply.github.com>
2020-06-28 17:53:09 +0300

    Update Refresher-exercise2.sql

--- refresher-exercises/Refresher-exercise2.sql
+++ refresher-exercises/Refresher-exercise2.sql
@@ -65,3 +65,9 @@ GROUP BY release_year
 HAVING COUNT(title) > 10;
 
 
+SELECT country, AVG(budget) as avg_budget, AVG(gross) as avg_gross
+FROM films
+GROUP BY country
+HAVING COUNT(title) > 10
+ORDER BY country
+LIMIT 5;
