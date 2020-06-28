69340c8c608c5c6333027874f58b12bf474b385a
disertus <61247961+disertus@users.noreply.github.com>
2020-06-28 15:18:11 +0300

    Practicing with aggregate functions

--- refresher-exercises/Refresher-exercise2.sql
+++ refresher-exercises/Refresher-exercise2.sql
@@ -11,3 +11,12 @@ FROM films
 WHERE title LIKE 'A%'
 AND release_year BETWEEN 2000 AND 2012;
 
+SELECT title, 
+        (gross - budget) AS net_profit -- usual arithmetic operations between values
+FROM films;
+SELECT AVG (duration) / 60.0 AS avg_duration_hours
+FROM films;
+
+-- calculating death rate in PERCENTAGE
+SELECT (COUNT(deathdate) * 100.0 / COUNT(*)) AS percentage_dead 
+FROM people;
