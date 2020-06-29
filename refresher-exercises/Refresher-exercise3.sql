b6e8182edf4c6cd70b3b0362d5232fe944d298a5
disertus <61247961+disertus@users.noreply.github.com>
2020-06-29 13:47:20 +0300

    Practicing INNER JOINS with CASE WHEN, THEN, ELSE, END

--- refresher-exercises/Refresher-exercise3.sql
+++ refresher-exercises/Refresher-exercise3.sql
@@ -119,7 +119,35 @@ SELECT name, continent, code, surface_area,
         ELSE 'small' END
         -- Alias name for the new field
         AS geosize_group
-FROM countries;
+FROM countries
+WHERE year = 2015;  -- results only for 2015
+
+-- INTO is used to save the current selection INTO the table named as the following alias 
+-- it is possible to create multiple separate selections, store them INTO separate tables, and combine them in another selection
+SELECT country_code, size,
+  CASE WHEN size > 50000000
+            THEN 'large'
+       WHEN size > 1000000
+            THEN 'medium'
+       ELSE 'small' END
+       AS popsize_group
+INTO pop_plus       
+FROM populations
+WHERE year = 2015;
+
+-- Select fields
+SELECT c.name,
+    c.continent,
+    c.geosize_group,
+    p.popsize_group
+-- 1. From countries_plus (alias as c)
+FROM countries_plus AS c
+  -- 2. Join to pop_plus from previous query (alias as p)
+  INNER JOIN pop_plus AS p
+    -- 3. Match on country code
+    ON c.code = p.country_code
+-- 4. Order the table    
+ORDER BY popsize_group, continent;
 
 
 
