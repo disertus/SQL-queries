4959bcabd9e0c432428f55a74c063b5aa2636703
disertus <61247961+disertus@users.noreply.github.com>
2020-06-29 12:05:36 +0300

    Practicing INNER JOINS

--- refresher-exercises/Refresher-exercise3.sql
+++ refresher-exercises/Refresher-exercise3.sql
@@ -1,5 +1,6 @@
 -- INNER and LEFT joins - two most common types of table joins
 -- KEY FIELD - used to reference one table to another
+-- PRIMARY KEYS are the unique identifiers, therefore can not have duplicates 
 
 -- INNER JOIN **only includes records in which the key is in both tables - other keys are ignored and left out**
 -- look up the matches in the right table corresponding to the key in the left table
@@ -9,3 +10,52 @@ SELECT p1.country, p1.continent,
 FROM prime_ministers AS p1
 INNER JOIN presidents AS p2  -- joins two tables having the same keys on 'country' key filed
 ON p1.country = p2.country
+
+SELECT cities.name AS city, 
+       countries.name AS country, 
+       countries.region
+FROM cities
+INNER JOIN countries
+    ON cities.country_code = countries.code;
+    
+SELECT c.code AS country_code, 
+        name, 
+        year, 
+        inflation_rate
+FROM countries AS c
+  INNER JOIN economies AS e
+    ON e.code = c.code;
+
+-- it is possible to COMBINE MULTIPLE JOINS in SQL. 
+-- It's useful to alias table names like their first letters to make queries more readable
+SELECT *
+FROM left_table
+  INNER JOIN right_table
+    ON left_table.id = right_table.id  -- joins the first 'right' table
+  INNER JOIN another_table
+    ON left_table.id = another_table.id;  -- joins the second 'right' table
+    
+SELECT c.code,
+    c.name, 
+    c.region, 
+    p.year,
+    p.fertility_rate
+FROM countries AS c
+INNER JOIN populations AS p
+ON p.country_code = c.code;
+
+
+-- !! if the field name is unique (not used in different tables), it can be referenced without the 'table.field' statement, just 'field'
+SELECT c.code, 
+    name, 
+    region, 
+    e.year, 
+    fertility_rate, -- unique names, no need for the table name preceding it
+    unemployment_rate
+  FROM countries AS c
+  INNER JOIN populations AS p
+    ON c.code = p.country_code
+  INNER JOIN economies AS e
+    ON c.code = e.code;
+    
+    
