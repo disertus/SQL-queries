ca2ed25b4a2cd745e686933f498955f82a4b9d5c
disertus <61247961+disertus@users.noreply.github.com>
2020-06-29 17:32:26 +0300

    Practicing different types of JOINs with filtering conditions

--- refresher-exercises/Refresher-exercise4.sql
+++ refresher-exercises/Refresher-exercise4.sql
@@ -36,3 +36,49 @@ GROUP BY c.region;
 
 -- RIGHT JOIN is much less common then inner or left JOINs
 -- It is the reverse of the LEFT JOIN
+
+
+-- FULL JOINs - selects all values for both LEFT and RIGHT table, despite lacking keys or values in any of those
+SELECT p1.country AS pm_co, p2.country AS pres_co,
+       prime_minister, president
+FROM prime_ministers AS p1
+FULL JOIN presidents AS p2
+USING (country);
+
+SELECT name AS country, code, region, basic_unit
+-- 1. From countries
+FROM countries
+  -- 2. Join to currencies
+  FULL JOIN currencies
+    -- 3. Match on code
+    USING (code)
+-- 4. Where region is North America or null
+WHERE region = 'North America' OR region IS null
+ORDER BY region;
+
+SELECT c.name, code, l.name AS language
+FROM languages AS l
+  FULL JOIN countries AS c
+    USING (code)
+-- Where countries.name starts with V or is null
+WHERE c.name LIKE 'V%' OR c.name IS NULL
+ORDER BY c.name;
+
+-- 7. Select fields
+SELECT c1.name AS country, region, l.name AS language,
+       basic_unit, frac_unit
+-- 1. From countries
+FROM countries AS c1
+  -- 2. Join with languages
+  FULL JOIN languages AS l
+  -- 3. Match on code
+    USING (code)
+  -- 4. Join with currencie
+  FULL JOIN currencies AS c2
+    -- 5. Match on code
+    USING (code)
+-- 6. Where region like Melanesia and Micronesia
+WHERE region LIKE 'Melanesia' OR region LIKE 'Micronesia';
+
+
+
