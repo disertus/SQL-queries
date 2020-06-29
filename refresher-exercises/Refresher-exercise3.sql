d19e42953fd0e9006f35edeb526d0ff32fe58d30
disertus <61247961+disertus@users.noreply.github.com>
2020-06-29 13:25:05 +0300

    Practicing CASE WHEN, THEN, ELSE, END statements

--- refresher-exercises/Refresher-exercise3.sql
+++ refresher-exercises/Refresher-exercise3.sql
@@ -1,5 +1,5 @@
 -- INNER and LEFT joins - two most common types of table joins
--- KEY FIELD - used to reference one table to another
+-- KEY FIELD - required to reference one table's values to another
 -- PRIMARY KEYS are the unique identifiers, therefore can not have duplicates 
 
 -- INNER JOIN **only includes records in which the key is in both tables - other keys are ignored and left out**
@@ -59,8 +59,8 @@ SELECT c.code,
     ON c.code = e.code;
     
     
--- INNER JOIN with USING
--- when a key firld has the same name in both tables it's possible to use USING instead of ON
+-- INNER JOIN with USING as a shortcut
+-- when a key field has the same name in both tables it's possible to use USING instead of ON
 SELECT l.id
        l.val
        r.val
@@ -68,5 +68,60 @@ FROM l
 INNER JOIN r
 USING (id); -- id field's name is the same in both tables, such wording is shorter and more readable
             -- the order of joins in this case does not play a big role
+            
+SELECT c.name AS country,
+    continent,
+    l.name AS language,
+    l.official
+  FROM countries AS c
+  INNER JOIN languages AS l
+    USING (code);
+
+-- SELF (inner) JOINS - when a table is joined with itself
+-- used to compare values from the field to other values from the same field of the same table
+SELECT p1.country AS country1,
+       p2.country as country2,
+       p1.continent
+FROM prime_ministers AS p1
+INNER JOIN prime_ministers AS p2
+ON p1.continent = p2.continent
+LIMIT 14;  -- query creates pairs - repeated country 1 -> all countries 2 which are on the same continent, including ITSELF AS COUNTRY2
+-- to fix the above, use:
+ON p1.continent = p2.continent AND p1.country <> p2.country
+
+
+-- CASE WHEN and THEN, ELSE, END (finally)
+-- equivalent to if-else statements in other programming languages
+SELECT name, continent, indep_year
+       CASE WHEN indep_year < 1900 THEN 'before 1900'  -- equivalent to 'if indep_year < 1900 print('before 1900')
+            WHEN indep_year <= THEN 'between 1900 and 1930'
+            ELSE 'after 1930' END
+            AS indep_year_group  -- puts everything from the above CASE staement into the 'indep_year_group' field
+FROM states
+ORDER BY indep_year_group;
+
+SELECT p1.country_code,
+       p1.size AS size2010,
+       p2.size AS size2015,
+       ((p2.size - p1.size)/p1.size * 100.0) AS growth_perc -- calculates the growth in population in percents for a given time period
+FROM populations as p1
+  INNER JOIN populations as p2
+    ON p1.country_code = p2.country_code
+        AND p1.year = (p2.year - 5); -- filters out the repeating results from self join
+
+
+SELECT name, continent, code, surface_area,
+    -- First case
+    CASE WHEN surface_area > 2000000 THEN 'large'
+        -- Second case
+        WHEN surface_area > 350000 THEN 'medium'
+        -- Else clause + end (finally)
+        ELSE 'small' END
+        -- Alias name for the new field
+        AS geosize_group
+FROM countries;
+
+
+
 
 
