1838ee2b0ad14c6776bd08ba7521fa0703af2d3b
disertus <61247961+disertus@users.noreply.github.com>
2020-06-30 12:05:27 +0300

    Practicing subqueries in WHERE and SELECT clauses

--- refresher-exercises/Refresher-exercise6.sql
+++ refresher-exercises/Refresher-exercise6.sql
@@ -13,3 +13,14 @@ SELECT DISTINCT continent
    FROM states
    WHERE prime_ministers.continent = states.continent) AS countries_num -- it's necessary to create an alias to name the field of a subquery
 FROM prime_ministers;
+
+-- 2. Select fields
+SELECT name, country_code, urbanarea_pop
+  -- 3. From cities
+  FROM cities
+-- 4. Where city name in the field of capital cities
+WHERE name IN
+  -- 1. Subquery
+  (SELECT capital
+   FROM countries)
+ORDER BY urbanarea_pop DESC;
