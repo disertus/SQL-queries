eb55dcb11b109d5fd5fb0990310d24f0b2f4227d
disertus <61247961+disertus@users.noreply.github.com>
2020-06-30 11:34:39 +0300

    Practicing Sub-queries inside WHERE and SELECT clauses

--- refresher-exercises/Refresher-exercise6.sql
+++ refresher-exercises/Refresher-exercise6.sql
@@ -1,2 +1,15 @@
 -- Subqueries inside WHERE statements
+SELECT name, fert_rate
+FROM states
+WHERE continent = 'Asia'
+  AND fert_rate <           -- this statement compares the fertility in asian states and selects only those
+    (SELECT AVG(fert_rate)  -- which are below the average for all states
+     FROM states);
 
+
+-- Subqueries inside SELECT clause
+SELECT DISTINCT continent
+  (SELECT COUNT(*)
+   FROM states
+   WHERE prime_ministers.continent = states.continent) AS countries_num -- it's necessary to create an alias to name the field of a subquery
+FROM prime_ministers;
