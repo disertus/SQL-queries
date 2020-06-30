42f39c1d1feeeeb77ecb0399cd16f824dd55db7d
disertus <61247961+disertus@users.noreply.github.com>
2020-06-30 15:54:18 +0300

    Collecting and learning query optimization tips

--- refresher-exercises/Query-optimization-tips.sql
+++ refresher-exercises/Query-optimization-tips.sql
@@ -5,4 +5,10 @@
 -- or, one can use the common table expressions (CTE), wrapped by a WITH statement - a standalone query returning a temporary result set
 -- more time- and resource-effective comparing to the subqueries
 
---
+-- Creating Temp tables can be useful, because they allow to perform several queries upon them (till the session is alive)
+-- this opposes them to CTEs, which disappear directly after they have been used
+CREATE TEMP TABLE tmp AS
+  SELECT holiday, holiday_types
+  FROM world_holidays
+  WHERE country_code = 'USA'
+ 
