0b773e6fbd1090d9a8a6e1bda206bb25c60bc705
disertus <61247961+disertus@users.noreply.github.com>
2020-06-30 15:47:42 +0300

    Practicing sql commands with subqueries and CTEs

--- refresher-exercises/Refresher-exercise7.sql
+++ refresher-exercises/Refresher-exercise7.sql
@@ -41,3 +41,23 @@ SELECT name, country_code, city_proper_pop, metroarea_pop,
        AND metroarea_pop IS NOT NULL  -- select records that are not NULL
 ORDER BY city_perc DESC
 LIMIT 10;  -- select only top 10 results
+
+-- CTE example, using the WITH clause to create a temporary table of records
+WITH south_cte AS -- CTE
+(
+  SELECT region
+    , ROUND(AVG(temp_06),2) AS avg_winter_temp
+    , ROUND(AVG(precip_06),2) AS avg_winter_precip
+  FROM oclimate
+  WHERE region IN ('Africa','South America','Australia and Oceania')
+  GROUP BY region
+)
+
+SELECT south.region, south.avg_winter_temp, south.avg_winter_precip
+  , COUNT(DISTINCT ath.athlete_id)
+FROM south_cte as south
+INNER JOIN athletes_recent ath
+  ON south.region = ath.region
+  AND ath.season = 'Winter'
+GROUP BY south.region, south.avg_winter_temp, south.avg_winter_precip
+ORDER BY south.avg_winter_temp;
