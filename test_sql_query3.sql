17107f3ba5b3d982b9f6ebe3285d71dfd9384e3c
disertus <61247961+disertus@users.noreply.github.com>
2020-06-30 12:57:26 +0300

    Update test_sql_query3.sql

--- test_sql_query3.sql
+++ test_sql_query3.sql
@@ -10,13 +10,18 @@ GROUP BY location_name, source_url  -- the query works because the non-aggregate
 ORDER BY location_name, frequency DESC;
 
 -- new solution
-SELECT news_posts.source_url,
-       grouped_table.location_name
-FROM news_posts INNER JOIN news_sources ON news_posts.source_url = news_sources.url
-GROUP BY news_sources.location_name
-HAVING COUNT (news_posts.source_url) = (
-            SELECT COUNT(news_posts.rource_url) AS post_count,
-                   news_sources.location_name
-            FROM news_posts
-            INNER JOIN news_sources ON news_posts.source_url = news_sources.url
-            GROUP BY news_sources.location_name) AS grouped_table
+SELECT news_sources.location_name,
+      (SELECT COUNT(source_url), 
+                    source_url
+       FROM news_posts
+       GROUP BY source_url)
+FROM news_sources
+      WHERE source_url IN 
+            (SELECT MAX(post_count) 
+             FROM (
+                   SELECT COUNT(news_posts.rource_url) AS post_count,
+                                news_sources.location_name
+                   FROM news_posts
+                   INNER JOIN news_sources ON news_posts.source_url = news_sources.url
+                   GROUP BY news_sources.location_name) AS grouped_table
+GROUP BY location_name;
