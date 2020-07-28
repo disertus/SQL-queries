947a37e146c9ba6bd6274d7373104811ee9e7268
disertus <61247961+disertus@users.noreply.github.com>
2020-07-28 14:31:51 +0300

    Update test_sql_query7.sql

--- test_sql_query7.sql
+++ test_sql_query7.sql
@@ -16,3 +16,22 @@ lifetime_per_user as (select
 select avg(user_lifetime)
 from lifetime_per_user as l
 where l.customer in (select stripe_id from cohort_users)
+
+
+--lifetime total
+with lifetime_per_paid_user as (
+  	select 
+	  	distinct p.id,
+		date_diff(max(date(period_end)), min(date(period_start)), month) as user_lifetime
+	from stripe.invoices as i
+  	inner join stripe.charges as c using (customer)
+  	inner join prod.users_view as p on i.customer = p.stripe_id -- users not divided into cohorts
+	where 
+		(period_start, period_end) is not null
+  		and c.amount > 100
+  		and c.refunded = false
+	group by p.id
+	having user_lifetime > 0)
+
+select round(avg(user_lifetime), 2)
+from lifetime_per_paid_user
